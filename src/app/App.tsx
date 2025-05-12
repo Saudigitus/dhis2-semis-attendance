72786ce6c2428b3dbcbe1cc1dba0168033100655
cnandza <cnandza@saudigitus.org>
2025-05-12 14:13:18 +0200

    preparing lib for publication

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { AppWrapper } from 'dhis2-semis-components'
 import { Router } from '../components/routes'
 
 const App = () => {
     return (
-        <AppWrapper dataStoreKey='semis/values'>
-            <Router/>
-        </AppWrapper>
+        <Router />
     )
 }
 
--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -6,12 +6,10 @@ import { Attendance } from '../../pages';
 export default function Router() {
 
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/' element={<FullLayout />}>
-                    <Route key={'attendance'} path={'/'} element={<Attendance />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route path='/' element={<FullLayout />}>
+                <Route key={'attendance'} path={'/'} element={<Attendance />} />
+            </Route>
+        </Routes>
     );
 }
