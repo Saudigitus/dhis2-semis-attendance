59ab6288c7a3a5f40416deed845d141617f17130
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-09-02 08:35:32 +0200

    refactor: comment code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { Router } from '../components/routes'
-import { HashRouter } from 'react-router-dom'
-import { AppWrapper } from 'dhis2-semis-components'
-import { useConfig } from '@dhis2/app-runtime'
+// import { HashRouter } from 'react-router-dom'
+// import { AppWrapper } from 'dhis2-semis-components'
+// import { useConfig } from '@dhis2/app-runtime'
 
 const App = () => {
-    const { baseUrl } = useConfig()
+    // const { baseUrl } = useConfig()
 
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey="dataStore/semis/values"
-            schoolCalendarKey='dataStore/semis/schoolCalendar'
-        >
-            <HashRouter>
+        // <AppWrapper
+        //     baseUrl={baseUrl}
+        //     dataStoreKey="dataStore/semis/values"
+        //     schoolCalendarKey='dataStore/semis/schoolCalendar'
+        // >
+        //     <HashRouter>
                 <Router />
-            </HashRouter >
-        </AppWrapper>
+        //     </HashRouter >
+        // </AppWrapper>
     )
 }
 
