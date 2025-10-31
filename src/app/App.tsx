9ab5f05241a5814ace27c623d3fe5ea08108f8ff
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-10-31 15:52:47 +0200

    refactor(App): simplify i18n handling and update dependencies
    - Remove unused translation import and fallback logic
    - Update dhis2-semis-components to version 0.6.8-alpha.35
    - Bump package version to 1.0.0-alpha.42

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,20 +4,21 @@ import { HashRouter } from 'react-router-dom'
 import { AppWrapper } from 'dhis2-semis-components'
 import { useConfig } from '@dhis2/app-runtime'
 import { D2I18n } from 'dhis2-semis-types'
-import translation from '../locales/index'
+// import translation from '../locales/index'
 
-const App = ({ i18n }: { i18n?: D2I18n }) => {
+const App = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()
-    const language = i18n == undefined ? translation : i18n
+    // const language = i18n == undefined ? translation : i18n
 
     return (
         // <AppWrapper
+        //     i18n={i18n}
         //     baseUrl={baseUrl}
         //     dataStoreKey="dataStore/semis/values"
         //     schoolCalendarKey='dataStore/semis/schoolCalendar'
         // >
         //     <HashRouter>
-                <Router i18n={language} />
+        <Router i18n={i18n} />
         //     </HashRouter >
         // </AppWrapper>
     )
