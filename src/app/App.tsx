62c5c87fd42a1ba5ff76aeaa7da2f5d3ac4c9b79
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-10-31 10:26:56 +0200

    refactor(App): simplify app component by removing wrapper components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,15 +11,15 @@ const App = ({ i18n }: { i18n?: D2I18n }) => {
     const language = i18n == undefined ? translation : i18n
 
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey="dataStore/semis/values"
-            schoolCalendarKey='dataStore/semis/schoolCalendar'
-        >
-            <HashRouter>
+        // <AppWrapper
+        //     baseUrl={baseUrl}
+        //     dataStoreKey="dataStore/semis/values"
+        //     schoolCalendarKey='dataStore/semis/schoolCalendar'
+        // >
+        //     <HashRouter>
                 <Router i18n={language} />
-            </HashRouter >
-        </AppWrapper>
+        //     </HashRouter >
+        // </AppWrapper>
     )
 }
 
